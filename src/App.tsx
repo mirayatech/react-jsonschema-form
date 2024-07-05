6e1f07aa38938fc98231cbb8b8136c01943cc5aa
mirayatech <mirayaabrodi@gmail.com>
2024-07-05 12:37:53 +0200

    feat: replace initial app setup with form component using RJSF

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Form from "@rjsf/core";
+import { RJSFSchema } from "@rjsf/utils";
+import validator from "@rjsf/validator-ajv8";
+
+const schema: RJSFSchema = {
+  title: "Person",
+  type: "object",
+  required: ["firstName", "lastName"],
+  properties: {
+    firstName: { type: "string", title: "First Name" },
+    lastName: { type: "string", title: "Last Name" },
+    age: { type: "integer", title: "Age" },
+  },
+};
+
+const log = (type: string) => console.log.bind(console, type);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Form
+      validator={validator}
+      schema={schema}
+      onChange={log("changed")}
+      onSubmit={log("submitted")}
+      onError={log("errors")}
+    />
+  );
 }
 
-export default App
+export default App;
