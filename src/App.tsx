898307b794d526a8897891b99d61f60b8838d483
mirayatech <mirayaabrodi@gmail.com>
2024-07-05 13:37:06 +0200

    feat: integrate MUI with RJSF form and add theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import Form from "@rjsf/core";
+import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import Form from "@rjsf/material-ui";
 import { RJSFSchema, UiSchema } from "@rjsf/utils";
 import validator from "@rjsf/validator-ajv8";
 import "./App.css";
@@ -15,25 +18,33 @@ const schema: RJSFSchema = {
 };
 
 const uiSchema: UiSchema = {
-  firstName: { "ui:classNames": "custom-class-first-name" },
-  lastName: { "ui:classNames": "custom-class-last-name" },
-  age: { "ui:classNames": "custom-class-age" },
+  firstName: {
+    "ui:widget": "text",
+    "ui:classNames": "custom-class-first-name",
+  },
+  lastName: { "ui:widget": "text", "ui:classNames": "custom-class-last-name" },
+  age: { "ui:widget": "updown", "ui:classNames": "custom-class-age" },
 };
 
 const log = (type: string) => console.log.bind(console, type);
 
+const theme = createTheme();
+
 function App() {
   return (
-    <div className="form-container">
-      <Form
-        validator={validator}
-        schema={schema}
-        uiSchema={uiSchema}
-        onChange={log("changed")}
-        onSubmit={log("submitted")}
-        onError={log("errors")}
-      />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="form-container">
+        <Form
+          validator={validator}
+          schema={schema}
+          uiSchema={uiSchema}
+          onChange={log("changed")}
+          onSubmit={log("submitted")}
+          onError={log("errors")}
+        />
+      </div>
+    </ThemeProvider>
   );
 }
 
